68001605d360f6c805673ae06ef3d2f2bfa8759b
Atsushi Watanabe <atsushi.w@ieee.org>
2019-05-02 14:11:37 +0900

    Update coding styles (#3)

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "ypspur_gui.h"
 #include <QApplication>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    QApplication app(argc, argv);
-    YPSpur_gui gui;
-    gui.show();
+  QApplication app(argc, argv);
+  YPSpurGUI gui;
+  gui.show();
 
-    return app.exec();
+  return app.exec();
 }
