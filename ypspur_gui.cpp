b7d420d92f4375ec590dbcb31be172617b59b292
Atsushi Watanabe <atsushi.w@ieee.org>
2019-05-02 13:17:06 +0900

    Update coding styles

--- ypspur_gui.cpp
+++ ypspur_gui.cpp
@@ -20,309 +20,305 @@
 #include <ntddser.h>
 #endif
 
-void printTextEdit(QTextEdit *out, QString str)
+void printTextEdit(QTextEdit* out, QString str)
 {
-    out->moveCursor(QTextCursor::End,QTextCursor::MoveAnchor);
-    out->insertHtml(str);
-    out->verticalScrollBar()->setValue(
-                out->verticalScrollBar()->maximum());
+  out->moveCursor(QTextCursor::End, QTextCursor::MoveAnchor);
+  out->insertHtml(str);
+  out->verticalScrollBar()->setValue(
+      out->verticalScrollBar()->maximum());
 }
 
-YPSpur_gui::YPSpur_gui(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::YPSpur_gui),
-    settings(QString("ypspur-gui"))
+YPSpurGUI::YPSpurGUI(QWidget* parent)
+  : QMainWindow(parent)
+  , ui_(new Ui::YPSpurGUI)
+  , settings_(QString("ypspur-gui"))
 {
-    ui->setupUi(this);
-    Qt::WindowFlags flags = Qt::Window | Qt::WindowMaximizeButtonHint |
-            Qt::WindowMinimizeButtonHint | Qt::WindowCloseButtonHint |
-            Qt::CustomizeWindowHint;
-    setWindowFlags(flags);
+  ui_->setupUi(this);
+  Qt::WindowFlags flags = Qt::Window | Qt::WindowMaximizeButtonHint |
+                          Qt::WindowMinimizeButtonHint | Qt::WindowCloseButtonHint |
+                          Qt::CustomizeWindowHint;
+  setWindowFlags(flags);
 
 #ifdef _WIN32
-	_tsetlocale(LC_ALL, _T(""));
+  _tsetlocale(LC_ALL, _T(""));
 
-    coordinatorPath = settings.value(
-                "coordinator/path",
-                "./ypspur-coordinator").toString();
-    interpreterPath = settings.value(
-                "interpreter/path",
-                "./ypspur-interpreter").toString();
-    devicePath = settings.value("coordinator/devicePath", "").toString();
-    deviceName = settings.value("coordinator/deviceName", "").toString();
+  coordinator_path_ = settings_.value("coordinator/path", "./ypspur-coordinator").toString();
+  interpreter_path_ = settings_.value("interpreter/path", "./ypspur-interpreter").toString();
+  device_path_ = settings_.value("coordinator/device_path", "").toString();
+  device_name_ = settings_.value("coordinator/device_name", "").toString();
 #else
-    coordinatorPath = settings.value(
-                "coordinator/path",
-                "/usr/local/bin/ypspur-coordinator").toString();
-    interpreterPath = settings.value(
-                "interpreter/path",
-                "/usr/local/bin/ypspur-interpreter").toString();
-    devicePath = settings.value("coordinator/devicePath", "/dev/").toString();
-    deviceName = settings.value("coordinator/deviceName", "ttyACM*").toString();
+  coordinator_path_ = settings_.value("coordinator/path", "/usr/local/bin/ypspur-coordinator").toString();
+  interpreter_path_ = settings_.value("interpreter/path", "/usr/local/bin/ypspur-interpreter").toString();
+  device_path_ = settings_.value("coordinator/device_path", "/dev/").toString();
+  device_name_ = settings_.value("coordinator/device_name", "ttyACM*").toString();
 #endif
-    ui->coordinatorPath->setText(coordinatorPath);
-    ui->interpreterPath->setText(interpreterPath);
+  ui_->coordinatorPath->setText(coordinator_path_);
+  ui_->interpreterPath->setText(interpreter_path_);
 
-    coordinatorOptions = settings.value("coordinator/options", "").toString();
-    interpreterOptions = settings.value("interpreter/options", "").toString();
-    ui->coordinatorOptions->setText(coordinatorOptions);
-    ui->interpreterOptions->setText(interpreterOptions);
+  coordinator_options_ = settings_.value("coordinator/options", "").toString();
+  interpreter_options_ = settings_.value("interpreter/options", "").toString();
+  ui_->coordinatorOptions->setText(coordinator_options_);
+  ui_->interpreterOptions->setText(interpreter_options_);
 
+  port_ = settings_.value("coordinator/port", "/dev/ttyACM0").toString();
+  if (!port_.isEmpty())
+    ui_->portList->addItem(port_);
 
-    port = settings.value("coordinator/port", "/dev/ttyACM0").toString();
-    if(!port.isEmpty()) ui->portList->addItem(port);
+  param_file_ = settings_.value("coordinator/param", "").toString();
+  QString paramName = param_file_;
+  paramName.replace(QRegExp("^.*([^/\\\\]*)$"), "\\1");
+  if (!param_file_.isEmpty())
+    ui_->parameterName->setText(paramName);
 
-    paramFile = settings.value("coordinator/param", "").toString();
-    QString paramName = paramFile;
-    paramName.replace(QRegExp("^.*([^/\\\\]*)$"),"\\1");
-    if(!paramFile.isEmpty()) ui->parameterName->setText(paramName);
+  connect(&coordinator_, SIGNAL(readyReadStandardError()), this, SLOT(updateCoordinatorError()));
+  connect(&coordinator_, SIGNAL(readyReadStandardOutput()), this, SLOT(updateCoordinatorText()));
+  connect(&coordinator_, SIGNAL(finished(int)), this, SLOT(coordinatorQuit(int)));
+  connect(&interpreter_, SIGNAL(readyReadStandardError()), this, SLOT(updateInterpreterError()));
+  connect(&interpreter_, SIGNAL(readyReadStandardOutput()), this, SLOT(updateInterpreterText()));
+  connect(&interpreter_, SIGNAL(finished(int)), this, SLOT(interpreterQuit(int)));
 
-    connect(&coordinator, SIGNAL(readyReadStandardError()), this, SLOT(updateCoordinatorError()));
-    connect(&coordinator, SIGNAL(readyReadStandardOutput()), this, SLOT(updateCoordinatorText()));
-    connect(&coordinator, SIGNAL(finished(int)), this, SLOT(coordinatorQuit(int)));
-    connect(&interpreter, SIGNAL(readyReadStandardError()), this, SLOT(updateInterpreterError()));
-    connect(&interpreter, SIGNAL(readyReadStandardOutput()), this, SLOT(updateInterpreterText()));
-    connect(&interpreter, SIGNAL(finished(int)), this, SLOT(interpreterQuit(int)));
-
-    ui->portList->installEventFilter(this);
+  ui_->portList->installEventFilter(this);
 }
 
-YPSpur_gui::~YPSpur_gui()
+YPSpurGUI::~YPSpurGUI()
 {
-    settings.sync();
-    delete ui;
+  settings_.sync();
+  delete ui_;
 }
 
-void YPSpur_gui::on_coordinatorStart_toggled(bool checked)
+void YPSpurGUI::on_coordinatorStart_toggled(bool checked)
 {
-    if(checked)
+  if (checked)
+  {
+    mutex_coordinator_output_.lock();
+    printTextEdit(ui_->coordinatorOut, "<hr style=\"width:100%;background-color:#CCCCCC;height:1pt;\">");
+    mutex_coordinator_output_.unlock();
+
+    ui_->parameterBrowse->setDisabled(true);
+    ui_->portList->setDisabled(true);
+
+    QStringList args;
+    args.append("--device");
+    args.append(port_);
+    if (!param_file_.isEmpty() && param_file_ != "embedded")
     {
-        mutexCoordinatorOutput.lock();
-        printTextEdit(ui->coordinatorOut, "<hr style=\"width:100%;background-color:#CCCCCC;height:1pt;\">");
-        mutexCoordinatorOutput.unlock();
-
-        ui->parameterBrowse->setDisabled(true);
-        ui->portList->setDisabled(true);
-
-        QStringList args;
-        args.append("--device");
-        args.append(port);
-        if(!paramFile.isEmpty() && paramFile != "embedded")
-        {
-            args.append("--param");
-            args.append(paramFile);
-        }
-        args.append("--update-param");
-        QStringList options = coordinatorOptions.split(QRegExp("\\s+"), QString::SkipEmptyParts);
-        if( !options.isEmpty() ) args.append(options);
-
-        coordinator.start(coordinatorPath, args);
+      args.append("--param");
+      args.append(param_file_);
     }
+    args.append("--update-param");
+    QStringList options = coordinator_options_.split(QRegExp("\\s+"), QString::SkipEmptyParts);
+    if (!options.isEmpty())
+      args.append(options);
+
+    coordinator_.start(coordinator_path_, args);
+  }
+  else
+  {
+    if (interpreter_.state() != QProcess::NotRunning)
+      interpreter_.close();
+    if (coordinator_.state() != QProcess::NotRunning)
+      coordinator_.close();
+    ui_->parameterBrowse->setDisabled(false);
+    ui_->portList->setDisabled(false);
+  }
+}
+
+void YPSpurGUI::coordinatorQuit(int exitCode)
+{
+  printTextEdit(ui_->coordinatorOut, "<br><i>Exit code: " + QString().number(exitCode) + "</i><br>");
+  ui_->coordinatorStart->setChecked(false);
+}
+
+void YPSpurGUI::interpreterQuit(int exitCode)
+{
+  printTextEdit(ui_->interpreterOut, "<br><i>Exit code: " + QString().number(exitCode) + "</i><br>");
+}
+
+void YPSpurGUI::updateCoordinatorError()
+{
+  mutex_coordinator_output_.lock();
+  QString data(coordinator_.readAllStandardError());
+
+  data.replace(QRegExp(" "), "&nbsp;");
+  data.replace(QRegExp("\n"), "<br>");
+  printTextEdit(ui_->coordinatorOut, "<b>" + data + "</b>");
+  mutex_coordinator_output_.unlock();
+  if (data.contains(QRegExp("Command&nbsp;analy[zs]er&nbsp;started\\.")))
+  {
+    mutex_interpreter_output_.lock();
+    printTextEdit(ui_->interpreterOut, "<hr style=\"width:100%;background-color:#CCCCCC;height:1pt;\">");
+    mutex_interpreter_output_.unlock();
+
+    QStringList options = interpreter_options_.split(QRegExp("\\s+"), QString::SkipEmptyParts);
+    if (!options.isEmpty())
+      interpreter_.start(interpreter_path_, options);
     else
-    {
-        if(interpreter.state() != QProcess::NotRunning) interpreter.close();
-        if(coordinator.state() != QProcess::NotRunning) coordinator.close();
-        ui->parameterBrowse->setDisabled(false);
-        ui->portList->setDisabled(false);
-    }
+      interpreter_.start(interpreter_path_);
+  }
 }
 
-void YPSpur_gui::coordinatorQuit(int exitCode)
+void YPSpurGUI::updateCoordinatorText()
 {
-    printTextEdit(ui->coordinatorOut, "<br><i>Exit code: " + QString().number(exitCode) + "</i><br>");
-    ui->coordinatorStart->setChecked(false);
+  mutex_coordinator_output_.lock();
+  QString data(coordinator_.readAllStandardOutput());
+
+  data.replace(QRegExp(" "), "&nbsp;");
+  data.replace(QRegExp("\n"), "<br>");
+  printTextEdit(ui_->coordinatorOut, data);
+  mutex_coordinator_output_.unlock();
 }
 
-void YPSpur_gui::interpreterQuit(int exitCode)
+void YPSpurGUI::updateInterpreterError()
 {
-    printTextEdit(ui->interpreterOut, "<br><i>Exit code: " + QString().number(exitCode) + "</i><br>");
+  mutex_interpreter_output_.lock();
+  QString data(interpreter_.readAllStandardError());
+
+  data.replace(QRegExp(" "), "&nbsp;");
+  data.replace(QRegExp("\n"), "<br>");
+  printTextEdit(ui_->interpreterOut, "<b>" + data + "</b>");
+  mutex_interpreter_output_.unlock();
 }
 
-void YPSpur_gui::updateCoordinatorError()
+void YPSpurGUI::updateInterpreterText()
 {
-    mutexCoordinatorOutput.lock();
-    QString data(coordinator.readAllStandardError() );
+  mutex_interpreter_output_.lock();
+  QString data(interpreter_.readAllStandardOutput());
 
-    data.replace(QRegExp(" "),"&nbsp;");
-    data.replace(QRegExp("\n"),"<br>");
-    printTextEdit(ui->coordinatorOut, "<b>" + data + "</b>");
-    mutexCoordinatorOutput.unlock();
-    if(data.contains(QRegExp("Command&nbsp;analy[zs]er&nbsp;started\\.")))
-    {
-        mutexInterpreterOutput.lock();
-        printTextEdit(ui->interpreterOut, "<hr style=\"width:100%;background-color:#CCCCCC;height:1pt;\">");
-        mutexInterpreterOutput.unlock();
-
-        QStringList options = interpreterOptions.split(QRegExp("\\s+"), QString::SkipEmptyParts);
-        if(!options.isEmpty())
-            interpreter.start(interpreterPath, options);
-        else
-            interpreter.start(interpreterPath);
-    }
+  data.replace(QRegExp(" "), "&nbsp;");
+  data.replace(QRegExp("\n"), "<br>");
+  printTextEdit(ui_->interpreterOut, data);
+  mutex_interpreter_output_.unlock();
 }
 
-void YPSpur_gui::updateCoordinatorText()
+void YPSpurGUI::on_parameterBrowse_clicked()
 {
-    mutexCoordinatorOutput.lock();
-    QString data(coordinator.readAllStandardOutput());
-
-    data.replace(QRegExp(" "),"&nbsp;");
-    data.replace(QRegExp("\n"),"<br>");
-    printTextEdit(ui->coordinatorOut, data);
-    mutexCoordinatorOutput.unlock();
+  QString defaultPath("./");
+  if (!param_file_.isEmpty())
+  {
+    defaultPath = param_file_;
+  }
+  QString fileName = QFileDialog::getOpenFileName(
+      this,
+      QString("Open Image"),
+      defaultPath,
+      QString("Parameter files (*.param)"));
+  if (!fileName.isEmpty())
+  {
+    setParamFile(fileName);
+  }
 }
 
-void YPSpur_gui::updateInterpreterError()
+void YPSpurGUI::setParamFile(QString fileName)
 {
-    mutexInterpreterOutput.lock();
-    QString data(interpreter.readAllStandardError() );
+  param_file_ = fileName;
 
-    data.replace(QRegExp(" "),"&nbsp;");
-    data.replace(QRegExp("\n"),"<br>");
-    printTextEdit(ui->interpreterOut, "<b>" + data + "</b>");
-    mutexInterpreterOutput.unlock();
-}
+  QString paramName = fileName;
+  paramName.replace(QRegExp("^.*([^/\\\\]*)$"), "\\1");
+  ui_->parameterName->setText(paramName);
 
-void YPSpur_gui::updateInterpreterText()
-{
-    mutexInterpreterOutput.lock();
-    QString data(interpreter.readAllStandardOutput());
-
-    data.replace(QRegExp(" "),"&nbsp;");
-    data.replace(QRegExp("\n"),"<br>");
-    printTextEdit(ui->interpreterOut, data);
-    mutexInterpreterOutput.unlock();
-}
-
-void YPSpur_gui::on_parameterBrowse_clicked()
-{
-    QString defaultPath("./");
-    if(!paramFile.isEmpty())
-    {
-        defaultPath = paramFile;
-    }
-    QString fileName = QFileDialog::getOpenFileName(
-                this,
-                QString("Open Image"),
-                defaultPath,
-                QString("Parameter files (*.param)"));
-    if(!fileName.isEmpty())
-    {
-        setParamFile(fileName);
-    }
-}
-
-void YPSpur_gui::setParamFile(QString fileName)
-{
-    paramFile = fileName;
-
-    QString paramName = fileName;
-    paramName.replace(QRegExp("^.*([^/\\\\]*)$"),"\\1");
-    ui->parameterName->setText(paramName);
-
-    settings.setValue("coordinator/param", paramFile);
+  settings_.setValue("coordinator/param", param_file_);
 }
 
 #if QT_VERSION >= 0x050000
-void YPSpur_gui::on_portList_currentTextChanged(const QString &arg1)
+void YPSpurGUI::on_portList_currentTextChanged(const QString& arg1)
 #else
-void YPSpur_gui::on_portList_textChanged(const QString &arg1)
+void YPSpurGUI::on_portList_textChanged(const QString& arg1)
 #endif
 {
-    if(!arg1.isNull())
+  if (!arg1.isNull())
+  {
+    QStringList options = arg1.split(" ", QString::SkipEmptyParts);
+    if (options.size() > 0)
     {
-        QStringList options = arg1.split(" ", QString::SkipEmptyParts);
-        if(options.size() > 0)
-        {
-	        port = options[0];
-	        settings.setValue("coordinator/port", port);
-	    }
+      port_ = options[0];
+      settings_.setValue("coordinator/port", port_);
     }
+  }
 }
 
-void YPSpur_gui::on_interpreterCommand_returnPressed()
+void YPSpurGUI::on_interpreterCommand_returnPressed()
 {
-    QString str = ui->interpreterCommand->text();
-    str.append("\n");
-    interpreter.write(str.toLocal8Bit());
-    ui->interpreterCommand->setText("");
+  QString str = ui_->interpreterCommand->text();
+  str.append("\n");
+  interpreter_.write(str.toLocal8Bit());
+  ui_->interpreterCommand->setText("");
 }
 
-bool YPSpur_gui::eventFilter(QObject *obj, QEvent *event)
+bool YPSpurGUI::eventFilter(QObject* obj, QEvent* event)
 {
-    if(event->type() == QEvent::MouseButtonPress)
-    {
+  if (event->type() == QEvent::MouseButtonPress)
+  {
 #ifdef _WIN32
-		HDEVINFO devInfo = SetupDiGetClassDevs(
-			NULL, NULL, NULL, DIGCF_PRESENT | DIGCF_ALLCLASSES);
-		TCHAR name[128];
-		TCHAR className[128];
-		TCHAR com[128];
-		SP_DEVINFO_DATA devData;
-		devData.cbSize = sizeof(SP_DEVINFO_DATA);
-		
-        ui->portList->clear();
-		for(int i = 0; SetupDiEnumDeviceInfo(devInfo, i, &devData); i++)
-		{
-			SetupDiGetDeviceRegistryProperty(
-				devInfo, &devData, SPDRP_CLASS, NULL, (BYTE*)className, sizeof(className), NULL);
-			if(_tcscmp(className, _T("Ports")) != 0) continue;
+    HDEVINFO devInfo = SetupDiGetClassDevs(
+        NULL, NULL, NULL, DIGCF_PRESENT | DIGCF_ALLCLASSES);
+    TCHAR name[128];
+    TCHAR className[128];
+    TCHAR com[128];
+    SP_DEVINFO_DATA devData;
+    devData.cbSize = sizeof(SP_DEVINFO_DATA);
 
-			SetupDiGetDeviceRegistryProperty(
-				devInfo, &devData, SPDRP_FRIENDLYNAME, NULL, (BYTE*)name, sizeof(name), NULL);
-			HKEY key = SetupDiOpenDevRegKey(devInfo, &devData,  DICS_FLAG_GLOBAL, 0, DIREG_DEV, KEY_QUERY_VALUE);
-			if(key)
-			{
-				DWORD len = sizeof(com);
-				RegQueryValueEx(key, _T("PortName"), NULL, NULL, (BYTE*)com, &len );
-				QString qname = QString::fromWCharArray(name);
-				qname.replace(QRegExp("\\s*\\(COM[0-9]+\\)\\s*$"),"");
-				ui->portList->addItem("\\\\.\\" + QString::fromWCharArray(com) + " " + qname);
-			}
-		}
-#else
-        QDir dir(devicePath, deviceName);
-        dir.setFilter(QDir::AllEntries | QDir::System);
-        dir.makeAbsolute();
+    ui_->portList->clear();
+    for (int i = 0; SetupDiEnumDeviceInfo(devInfo, i, &devData); i++)
+    {
+      SetupDiGetDeviceRegistryProperty(
+          devInfo, &devData, SPDRP_CLASS, NULL, (BYTE*)className, sizeof(className), NULL);
+      if (_tcscmp(className, _T("Ports")) != 0)
+        continue;
 
-        QFileInfoList list = dir.entryInfoList();
-        ui->portList->clear();
-        for(int i = 0; i < list.count(); i++)
-        {
-            ui->portList->addItem(list[i].absoluteFilePath());
-        }
-#endif
+      SetupDiGetDeviceRegistryProperty(
+          devInfo, &devData, SPDRP_FRIENDLYNAME, NULL, (BYTE*)name, sizeof(name), NULL);
+      HKEY key = SetupDiOpenDevRegKey(devInfo, &devData, DICS_FLAG_GLOBAL, 0, DIREG_DEV, KEY_QUERY_VALUE);
+      if (key)
+      {
+        DWORD len = sizeof(com);
+        RegQueryValueEx(key, _T("PortName"), NULL, NULL, (BYTE*)com, &len);
+        QString qname = QString::fromWCharArray(name);
+        qname.replace(QRegExp("\\s*\\(COM[0-9]+\\)\\s*$"), "");
+        ui_->portList->addItem("\\\\.\\" + QString::fromWCharArray(com) + " " + qname);
+      }
     }
-    fflush(stderr);
-    return QObject::eventFilter(obj, event);
+#else
+    QDir dir(device_path_, device_name_);
+    dir.setFilter(QDir::AllEntries | QDir::System);
+    dir.makeAbsolute();
+
+    QFileInfoList list = dir.entryInfoList();
+    ui_->portList->clear();
+    for (int i = 0; i < list.count(); i++)
+    {
+      ui_->portList->addItem(list[i].absoluteFilePath());
+    }
+#endif
+  }
+  fflush(stderr);
+  return QObject::eventFilter(obj, event);
 }
 
-
-void YPSpur_gui::on_coordinatorDefaultParam_clicked()
+void YPSpurGUI::on_coordinatorDefaultParam_clicked()
 {
-    setParamFile("embedded");
+  setParamFile("embedded");
 }
 
-void YPSpur_gui::on_coordinatorPath_textChanged(const QString &arg1)
+void YPSpurGUI::on_coordinatorPath_textChanged(const QString& arg1)
 {
-    coordinatorPath = arg1;
-    settings.setValue("coordinator/path", arg1);
+  coordinator_path_ = arg1;
+  settings_.setValue("coordinator/path", arg1);
 }
 
-void YPSpur_gui::on_coordinatorOptions_textChanged(const QString &arg1)
+void YPSpurGUI::on_coordinatorOptions_textChanged(const QString& arg1)
 {
-    coordinatorOptions = arg1;
-    settings.setValue("coordinator/options", arg1);
+  coordinator_options_ = arg1;
+  settings_.setValue("coordinator/options", arg1);
 }
 
-void YPSpur_gui::on_interpreterPath_textChanged(const QString &arg1)
+void YPSpurGUI::on_interpreterPath_textChanged(const QString& arg1)
 {
-    interpreterPath = arg1;
-    settings.setValue("interpreter/path", arg1);
+  interpreter_path_ = arg1;
+  settings_.setValue("interpreter/path", arg1);
 }
 
-void YPSpur_gui::on_interpreterOptions_textChanged(const QString &arg1)
+void YPSpurGUI::on_interpreterOptions_textChanged(const QString& arg1)
 {
-    interpreterOptions = arg1;
-    settings.setValue("interpreter/options", arg1);
+  interpreter_options_ = arg1;
+  settings_.setValue("interpreter/options", arg1);
 }
--- ypspur_gui.h
+++ ypspur_gui.h
@@ -14,64 +14,63 @@
 #include <QMutex>
 #include <QSettings>
 
-namespace Ui {
-class YPSpur_gui;
+namespace Ui
+{
+class YPSpurGUI;
 }
 
-
-class YPSpur_gui : public QMainWindow
+class YPSpurGUI : public QMainWindow
 {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    explicit YPSpur_gui(QWidget *parent = 0);
-    ~YPSpur_gui();
-    void setParamFile(QString param);
+  explicit YPSpurGUI(QWidget* parent = 0);
+  ~YPSpurGUI();
+  void setParamFile(QString param);
 
 private slots:
-    void on_coordinatorStart_toggled(bool checked);
-    void updateCoordinatorError();
-    void updateCoordinatorText();
-    void updateInterpreterError();
-    void updateInterpreterText();
+  void on_coordinatorStart_toggled(bool checked);
+  void updateCoordinatorError();
+  void updateCoordinatorText();
+  void updateInterpreterError();
+  void updateInterpreterText();
 
-    void interpreterQuit(int exitCode);
-    void coordinatorQuit(int exitCode);
+  void interpreterQuit(int exitCode);
+  void coordinatorQuit(int exitCode);
 
-    void on_parameterBrowse_clicked();
+  void on_parameterBrowse_clicked();
 #if QT_VERSION >= 0x050000
-    void on_portList_currentTextChanged(const QString &arg1);
+  void on_portList_currentTextChanged(const QString& arg1);
 #else
-    void on_portList_textChanged(const QString &arg1);
+  void on_portList_textChanged(const QString& arg1);
 #endif
-    void on_interpreterCommand_returnPressed();
-    void on_coordinatorDefaultParam_clicked();
-    void on_coordinatorPath_textChanged(const QString &arg1);
-    void on_coordinatorOptions_textChanged(const QString &arg1);
-    void on_interpreterPath_textChanged(const QString &arg1);
-    void on_interpreterOptions_textChanged(const QString &arg1);
+  void on_interpreterCommand_returnPressed();
+  void on_coordinatorDefaultParam_clicked();
+  void on_coordinatorPath_textChanged(const QString& arg1);
+  void on_coordinatorOptions_textChanged(const QString& arg1);
+  void on_interpreterPath_textChanged(const QString& arg1);
+  void on_interpreterOptions_textChanged(const QString& arg1);
 
 private:
-    bool eventFilter(QObject *obj, QEvent *event);
+  bool eventFilter(QObject* obj, QEvent* event);
 
+  Ui::YPSpurGUI* ui_;
 
-    Ui::YPSpur_gui *ui;
+  QProcess interpreter_;
+  QProcess coordinator_;
+  QMutex mutex_interpreter_output_;
+  QMutex mutex_coordinator_output_;
 
-    QProcess interpreter;
-    QProcess coordinator;
-    QMutex mutexInterpreterOutput;
-    QMutex mutexCoordinatorOutput;
+  QString param_file_;
+  QString port_;
+  QString coordinator_path_;
+  QString interpreter_path_;
+  QString coordinator_options_;
+  QString interpreter_options_;
+  QString device_path_;
+  QString device_name_;
 
-    QString paramFile;
-    QString port;
-    QString coordinatorPath;
-    QString interpreterPath;
-    QString coordinatorOptions;
-    QString interpreterOptions;
-    QString devicePath;
-    QString deviceName;
-
-    QSettings settings;
+  QSettings settings_;
 };
 
-#endif // YPSPUR_GUI_H
+#endif  // YPSPUR_GUI_H
